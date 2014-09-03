d0bcd5e8c1565ef449d196c5d9c41c618ef60662
Tristan Wietsma <tristan.wietsma@gmail.com>
2014-09-02 20:59:58 -0500

    added close and isclose

--- connection.go
+++ connection.go
@@ -1,9 +1,13 @@
 package netpool
 
-import "net"
+import (
+	"net"
+	"time"
+)
 
 type Connection struct {
-	conn net.Conn
+	conn     net.Conn
+	IsClosed bool
 }
 
 // Returns a connection to `address`.
@@ -12,16 +16,31 @@ func NewConnection(network, address string) (*Connection, error) {
 	c := Connection{
 		conn: conn,
 	}
+	if err == nil {
+		c.IsClosed = true
+	}
 	return &c, err
 }
 
 func (c *Connection) Write(msg []byte) error {
 	_, err := c.conn.Write(msg)
+	if err != nil {
+		c.IsClosed = true
+	}
 	return err
 }
 
 func (c *Connection) Read() ([]byte, error) {
 	buf := make([]byte, 1024)
 	nb, err := c.conn.Read(buf)
+	if err != nil {
+		c.IsClosed = true
+	}
 	return buf[:nb], err
+}
+
+func (c *Connection) Close() error {
+	err := c.conn.Close()
+	c.IsClosed = true
+	return err
 }
